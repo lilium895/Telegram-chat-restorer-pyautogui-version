6debb2444ee64611beec7ad936c20c9164dd5c99
lilium895 <56188107+lilium895@users.noreply.github.com>
2024-04-30 12:57:57 +0200

    Add files via upload

---
+++ html_to_txt_pyautogui.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+
+def transform_html_to_whatsapp(html_file, counting):
+    
+    
+    # Read the HTML file
+    with open(html_file, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Extract chat messages
+    messages = soup.find_all('div', class_='message')
+
+    # Transform messages to WhatsApp format
+    whatsapp_chat = ''
+    for message in messages:
+        
+        #Name of sender
+        sender_element = message.find('div', class_='from_name')
+        if sender_element is  not None:
+            sender = sender_element.text.strip()
+
+        #date and time
+        timestamp_div = message.find('div', class_="pull_right date details")
+        if timestamp_div:
+            timestamp = timestamp_div['title']
+            date_str = timestamp[:10]
+            time_str = timestamp[11:19]
+        else:
+            date_str, time_str = None, None
+        
+        #find if it is a reply
+        reply_element=message.find('div',class_='reply_to details')
+        replyto = None
+        if reply_element is not None:
+            for number in reply_element.find_all('a'):
+                replyto=number.get('href')
+                
+                #remove text before number of replied message
+                i=replyto.index("go_to")
+                replyto=replyto[i+13:]
+                a=int(replyto)
+                actual_message_number = message.get('id')
+                i=actual_message_number.index("m")
+                actual_message_number=actual_message_number[i+7:]
+                b=int(actual_message_number)
+                # Format reply in WhatsApp format
+                if time_str:
+                    whatsapp_message = f'{sender}\t{date_str}, {time_str}\t'
+                    if replyto is not None:
+                        whatsapp_message += f'In reply to message {a-b}\n'
+                    whatsapp_chat += whatsapp_message
+            
+        
+        #find text message
+        text_element = message.find('div', class_='text')
+        if text_element is not None:
+            text=text_element.text.strip()
+            
+            # Format txt message in WhatsApp format
+            if time_str:
+                whatsapp_message = f'{sender}\t{date_str}, {time_str}\t'
+                if text is not None:
+                    whatsapp_message += f'{text}\n'
+                whatsapp_chat += whatsapp_message
+        #find media
+        media_element=message.find('div',class_='media_wrap clearfix')
+        media = None
+        if media_element is not None:
+            for link in media_element.find_all('a'):
+                media=link.get('href')
+                if 'https' not in media:
+                    #remove chat_01 before file name
+                    i=media.index("chat_")
+                    media=media[i+8:]                
+                    #remove filetype before file name
+                    i=media.index("/")
+                    media=media[i+1:]
+                
+                # Format media message in WhatsApp format
+                if time_str:
+                    whatsapp_message = f'{sender}\t{date_str}, {time_str}\t'
+                    if media is not None:
+                        if 'https' in media:
+                            whatsapp_message += f'{media}\n'
+                        else:
+                            whatsapp_message += f'mediaelement\t{media}\n'
+                            #list of media to double check if you have 'em
+                            '''
+                            with open('media_list.txt', 'a+', encoding='utf-8') as medialist:
+                                medialist.write(f'{media}\t{media_counter}\n')
+                            media_counter+=1
+                            '''
+                    whatsapp_chat += whatsapp_message
+                                                
+            
+            #stickers or animation not found            
+            status_emoji=media_element.find('div',class_='status details')
+            if status_emoji is not None:
+                sticker_emoji=status_emoji.text.strip()
+                description=media_element.find('div', class_='title bold')
+                whatismedia=description.text.strip()
+                #check if it's a location
+                if whatismedia == 'Location' or whatismedia==f'{media}':
+                    continue                
+                else:
+                    if time_str:
+                        whatsapp_message = f'{sender}\t{date_str}, {time_str}\t'
+                        if sticker_emoji is not None:                            
+                            whatsapp_message += f'{whatismedia}: {sticker_emoji}\n'
+                            
+                            whatsapp_chat += whatsapp_message
+               
+
+    # Save the transformed chat to a file
+    chat_name='_chat{}.txt'.format(counting)
+    with open(chat_name, 'w', encoding='utf-8') as file:
+        file.write(whatsapp_chat)
+
+    print('Transformation complete. The WhatsApp chat export is saved as {}'.format(chat_name))
+
+# Usage example
+print('Where are the html files located? Copy and paste the path file')
+messages_path=input()
+messages_path=messages_path.strip('\"')+'\\'
+
+# message.html
+counter=1
+file_path = f"{messages_path}messages.html"
+transform_html_to_whatsapp(file_path,counter)
+
+# messagesCOUNTER.html
+for counter in range(256):
+    if counter == 1 or counter == 0: continue
+    file_path = f"{messages_path}messages{counter}.html"
+    transform_html_to_whatsapp(file_path,counter)
---
+++ pyautogui_recover.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 18:03:26 2024 
+
+@author: lilium895
+"""
+import os
+import time
+import pyautogui
+import pyperclip
+
+
+#parsing_message[0] is the sender
+#parsing_message[1] is date and time
+#parsing_message[2] is the text and the flag for the media element
+#parsing_message[3] is the name of the media
+#list_of_parsed[][] the first value is the message, the second one is the parsed data
+
+#change to second sender
+def change_to_second():
+    pyautogui.moveTo(x=1492, y=604)
+    pyautogui.click()
+#change to first sender
+def change_to_first():
+    pyautogui.moveTo(x=548, y=603)
+    pyautogui.click()
+    
+    
+
+
+
+
+def write_txt(txt,date,sender):
+    pyperclip.copy(txt)
+    pyautogui.hotkey("ctrl", "v")
+    time.sleep(0.25)
+    pyautogui.keyDown('shift')
+    pyautogui.press('enter')
+    pyautogui.keyUp('shift')
+    pyperclip.copy(date)
+    pyautogui.hotkey("ctrl", "v")    
+    pyautogui.keyDown('shift')
+    pyautogui.press('left', presses=20)
+    pyautogui.keyUp('shift')
+    time.sleep(0.25)
+    pyautogui.hotkey('ctrl','shift','m')
+    time.sleep(0.25)
+    pyautogui.press('enter')
+ 
+def search_for_media(media_name,user):
+    pyautogui.click(x=1776, y=713)
+    pyautogui.hotkey("ctrl", "a")
+    pyautogui.press('delete')
+    time.sleep(1)
+    pyperclip.copy(media_name)
+    pyautogui.hotkey("ctrl", "v")
+    time.sleep(1)
+    pyautogui.moveTo(x=1716, y=855)
+    pyautogui.click()
+    time.sleep(0.7)
+    if user==1:
+        pyautogui.mouseDown(button='left')
+        pyautogui.moveTo(x=548, y=343, duration=1)
+        pyautogui.mouseUp(button='left')
+    else:
+        pyautogui.mouseDown(button='left')
+        pyautogui.moveTo(x=1492, y=344, duration=1)
+        pyautogui.mouseUp(button='left')
+    time.sleep(1)
+    pyautogui.press('shift')    
+    pyautogui.press('enter')
+    media_size=os.path.getsize(f"C:\\Users\\gabri\\Desktop\\progetto telegram\\Lari definitivo\\Telegram\\allmedia\\{media_name}")
+    if media_size < 8000000:
+        time.sleep(2)
+    else:
+        time.sleep(120)
+
+
+
+
+'''
+IMPORTANT
+REPLACE 'number of text files' with the last number that appear in _chat{number}
+but with +1
+'''
+
+#insert the number of text files to recover
+
+for number in range(1,number of text files):
+    print(number)
+    change_to_second()
+    user1=3
+    user2=1
+'''
+IMPORTANT
+replace C:\\Users\\pollicino with the path of the folder in which are the _chat files
+leave\\_chat{number}.txt after
+'''
+    with open(f"C:\\Users\\pollicino\\_chat{number}.txt", 'r', encoding='utf-8') as file:
+   #remove \n and makes a list of messages
+       raw_messages = file.read().splitlines() 
+       number_of_messages=len(raw_messages)
+    list_of_parsed_messages=[]
+    for element in raw_messages:
+    #makes a lists of the constitutive elements of the message
+        message=list(element.split("\t"))
+        sender=message[0]
+        date_time=message[1]
+        text_or_flag=message[2]
+        
+        '''
+        IMPORTANT
+        REPLACE Sender name with the name of one of the two senders
+        
+        '''
+        if 'Sender name' in sender:
+            user1=3     
+            change_to_second()
+        else:
+            user1=1
+            change_to_first()
+        if text_or_flag == 'mediaelement':
+            media=message[3]
+            search_for_media(media,user1)
+
+        else:
+            write_txt(text_or_flag,date_time,user1) 
+
+
